7d2d10a3ad850cff62e9ab831f6917939562bbbd
James-Kitson <jameskitson10@gmail.com>
2016-07-07 14:25:56 +0100

    got the plotting working

--- Rscripts/plot_density.R
+++ Rscripts/plot_density.R
@@ -4,14 +4,20 @@ rm(list=ls())
 
 library(ggmap)#Load libraries
 library(ggplot2)
+library(dplyr)
 
-positions <- data.frame(lon=rnorm(20000, mean=-75.1803458, sd=0.05),
-                        lat=rnorm(10000,mean=39.98352197, sd=0.05))#Simulate some geographical coordinates #Switch out for your data that has real GPS coords
-map <- get_map(location=c(lon=-75.1803458,
-                          lat=39.98352197), zoom=11, maptype='roadmap', color='bw')#Get the map from Google Maps
+### Read in the position data, this data requires considerable cleaning to be useful.
+position.data <- read.csv("Data/Sample_locations.csv", header = TRUE, stringsAsFactors = FALSE)
+position.data.subs<-filter(position.data, Dec.Longitude != "unknown" & Island == "La_Reunion")
+positions <- data.frame(lon=as.numeric(position.data.subs$Dec.Longitude),
+                        lat=as.numeric(position.data.subs$Dec.Latitude))
+
+map <- get_map(location=c(lon=55.533340,
+                          lat=-21.123586), zoom=10, maptype='roadmap', color='color', scale=2)#Get the map from Google Maps
 ggmap(map, extent = "device") +
   geom_density2d(data = positions, aes(x = lon, y = lat), size = 0.3) +
   stat_density2d(data = positions,
                  aes(x = lon, y = lat, fill = ..level.., alpha = ..level..), size = 0.01,
-                 bins = 16, geom = "polygon") + scale_fill_gradient(low = "green", high = "red") +
-  scale_alpha(range = c(0, 0.3), guide = FALSE)#Plot
+                 bins = 25, geom = "polygon") + scale_fill_gradient(low = "green", high = "red") +
+  scale_alpha(range = c(0.1, 0.3), guide = FALSE) +
+  geom_point(data = positions, aes(x = lon, y = lat))
